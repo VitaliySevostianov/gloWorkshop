7529a8031a0751f02b06bc6fb3af61eb8ed09fc4
Vitaliy <vit.sev@mail.ru>
2019-10-10 19:49:17 +0300

    small animation when modal appears

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     //Screen keyboard
     {
         const keyboardBtn = document.querySelector('.search-form__keyboard');
@@ -12,13 +12,43 @@ document.addEventListener("DOMContentLoaded", () => {
             keyboard.style.top = keyboard.style.top ? '' : '50%';
         }
 
-        const typing = event => {  
-            console.log(event.target.innerText);  
-            if(event.target.innerText == "⬅"){
-                searchInput.value = searchInput.value.slice(0, -1);
+        const changeLang = (btn, lang) => {
+            const langRu = ['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+                'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',
+                'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',
+                'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.',
+                'en', ' '
+            ];
+            const langEn = ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"',
+                'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+                'ru', ' '
+            ];
+            if (lang == 'en') {
+                btn.forEach((elem, i) => {
+                    elem.innerText = langEn[i];
+                })
+            } else {
+                btn.forEach((elem, i) => {
+                    elem.innerText = langRu[i];
+                })
             }
-            else if(event.target.tagName === 'BUTTON'){
-                searchInput.value += event.target.innerText;
+        };
+
+
+        const typing = event => {
+            if (event.target.tagName === 'BUTTON') {
+                const buttons = [...keyboard.querySelectorAll('button')]
+                    .filter(elem => elem.style.visibility !== 'hidden');
+                console.log(buttons);
+                if (event.target.innerText == "⬅") {
+                    searchInput.value = searchInput.value.slice(0, -1);
+                } else if (event.target.innerText === 'en' || event.target.innerText === 'ru') {
+                    changeLang(buttons, event.target.innerText)
+                } else {
+                    searchInput.value += event.target.innerText;
+                }
             }
         }
 
@@ -41,16 +71,78 @@ document.addEventListener("DOMContentLoaded", () => {
             let target = event.target
             target = target.closest('a[href ="#"]');
 
-            if(target){
+            if (target) {
                 const parentTarget = target.parentElement;
                 console.log(sidebarMenu.querySelectorAll('li').forEach(elem => {
-                    if(elem === parentTarget){
+                    if (elem === parentTarget) {
                         elem.classList.add('active');
-                    }else{
+                    } else {
                         elem.classList.remove('active');
                     }
                 }));
             }
         });
+    }
+
+    //Modal
+    {
+        document.body.insertAdjacentHTML('beforeend',
+                                                    `<div class="youTuberModal">
+                                                        <div id="youtuberClose">&#215;</div>
+                                                        <div id="youtuberContainer"></div>
+                                                    </div>`);
+
+        const youtuberItems = document.querySelectorAll('[data-youtuber]');
+        const youtuberModal = document.querySelector('.youTuberModal');
+        const youtuberContainer = document.getElementById('youtuberContainer')
+
+        const qw = [3840, 2560, 1920, 1280, 854, 640, 426, 256],
+            qh = [2160, 1440, 1080, 720, 480, 360, 240, 144];
+
+
+        const sizeVideo = () => {
+            let ww = document.documentElement.clientWidth,
+                wh = document.documentElement.clientHeight;
+
+            for (let i = 0; i < qw.length; i++) {
+                if (ww > qw[i]) {
+                    youtuberContainer.querySelector('iframe').style.cssText = `
+                            width: ${qw[i]}px;
+                            height: ${qh[i]}px;
+                        `;
+                    youtuberContainer.style.cssText = `
+                            width: ${qw[i]}px;
+                            height: ${qh[i]}px;
+                            top: ${(wh - qh[i]) / 2}px;
+                            left: ${(ww - qw[i]) / 2}px;
+                        `;
+                    break;
+                }
+            }
+        };
+
+        youtuberItems.forEach(elem => {
+            elem.addEventListener('click', () => {
+                const idVideo = elem.dataset.youtuber;
+                youtuberModal.style.display = "block";
+
+                const youtuberFrame = document.createElement('iframe')
+                youtuberFrame.src = `https://youtube.com/embed/${idVideo}`;
+                youtuberContainer.insertAdjacentElement('beforeend', youtuberFrame);
+                sizeVideo();
+            });
+        });
+
+        youtuberModal.addEventListener('click', () => {
+            youtuberModal.style.display = '';
+            youtuberContainer.textContent = '';
+            window.removeEventListener('resize', sizeVideo);
+        });
+    }
+
+    //Youtube
+    {
+        const API_KEY = 'AIzaSyCEFg-w7uVtTV7fyLUWmt2XTWajin3FlTg';
+        const CLIENT_ID = '180348839409-qugal2968hp8737d24aj2p2od2ft1nir.apps.googleusercontent.com';
     }
 });
